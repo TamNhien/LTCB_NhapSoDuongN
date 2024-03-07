179e675782e4043431c185d0af9c0386741d80a9
TamNhien <canhthien91@gmail.com>
2024-03-07 19:15:22 +0700

    Add project files.

---
+++ LTCB_NhapSoDuongN.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Nhap so luong cac so duong: ";
+    cin >> n;
+
+    cout << "Nhap " << n << " so duong :\n";
+    for (int i = 0, num; i < n && cin >> num && num >= 0; ++i) {}
+
+    return 0;
+}
